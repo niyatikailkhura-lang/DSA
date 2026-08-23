526953e3848eb10dd316d88606f5e45554d0feff
Niyati Kailkhura <niyatikailkhura@gmail.com>
2026-08-23 19:06:42 +0530

    second method

--- binary trees/check_balanced binary tree.cpp
+++ binary trees/check_balanced binary tree.cpp
@@ -32,3 +32,36 @@ int lefth(TreeNode*node){
         return true;
     }
 };
+
+//method 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int s=1;
+int lefth(TreeNode*node){
+    if(node==NULL){
+        return 0;
+    }
+    int left=lefth(node->left);
+    if(left==-1) return -1;
+    int right=lefth(node->right);
+       if(right==-1) return -1;
+     if(abs(left - right) > 1) return -1;
+    int height=1+max(left,right);
+   
+   return height;
+}
+    bool isBalanced(TreeNode* root) {
+       return lefth(root)!=-1;
+    }
+};
