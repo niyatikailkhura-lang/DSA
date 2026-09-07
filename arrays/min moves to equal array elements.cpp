d8363bfb406e7c407dd5055c1feeb8ab81163138
Niyati Kailkhura <niyatikailkhura@gmail.com>
2026-09-07 08:54:24 +0530

    array

--- arrays/min moves to equal array elements.cpp
+++ arrays/min moves to equal array elements.cpp
@@ -1,3 +1,8 @@
+//Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+//In one move, you can increment n - 1 elements of the array by 1.
+
+ 
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
