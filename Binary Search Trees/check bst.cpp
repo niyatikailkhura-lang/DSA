e6d1378de35f0efc7c1aa6ea2986fd2cfe2672ba
Niyati Kailkhura <niyatikailkhura@gmail.com>
2026-09-26 11:32:50 +0530

    check bst

---
+++ Binary Search Trees/check bst.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+ bool checkbst(TreeNode*root,long long min,long long max){
+ if(root==NULL)return true;
+ if(root->val<=min||root->val>=max) return false;
+ return checkbst(root->left,min,root->val) && checkbst(root->right,root->val,max);
+ }
+    bool isValidBST(TreeNode* root) {
+        return checkbst(root,LLONG_MIN,LLONG_MAX);
+    }
+};
